e42fb715fab9cc9f8a7030f4c475017755971c22
David Rodriguez <dissonant.tech@gmail.com>
2016-07-11 15:08:01 -0400

    Add djrfdocs and Add User and Group Serializers.
    drfdocs generate documentation from django and can be found at the /docs
    url.

--- splttr/urls.py
+++ splttr/urls.py
@@ -13,11 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
 from rest_framework.authtoken import views as authview
+from splttr import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^users/login', authview.obtain_auth_token),
     url(r'^admin', admin.site.urls),
-    url(r'^login', authview.obtain_auth_token),
+    url(r'^docs', include('rest_framework_docs.urls')),
 ]
---
+++ splttr/views.py
@@ -0,0 +1,35 @@
+from rest_framework import status, viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.contrib.auth.models import User, Group
+
+from splttr.serializers import UserSerializer, GroupSerializer
+
+@api_view(['POST'])
+def register_user(request):
+    serialized = UserSerializer(data=request.DATA)
+    if serialized.is_valid():
+        User.objects.create_user(
+            serialized.init_data['email'],
+            serialized.init_data['username'],
+            serialized.init_data['password']
+        )
+        return Response(serialized.data, status=status.HTTP_201_CREATED)
+    else:
+        return Response(serialized._errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
